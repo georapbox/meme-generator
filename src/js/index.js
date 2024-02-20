d2d0fdbe6c633af5571d371d34d3cc469acf6018
georapbox <georapbox@gmail.com>
2024-02-20 09:47:46 +0200

    Add cancel button in remove text modal

--- src/js/index.js
+++ src/js/index.js
@@ -373,6 +373,12 @@ const handleTextRemoveFormSubmit = evt => {
   }
 };
 
+const removeConfirmationModalClose = evt => {
+  if (evt.target?.closest('[data-close-modal]') !== null) {
+    removeConfirmationModal.open = false;
+  }
+};
+
 const handleInputsContainerPointerdown = evt => {
   const element = evt.target;
   const textBoxEl = element.closest('[data-section="textBox"]');
@@ -519,6 +525,7 @@ document.addEventListener('capture-photo:success', handleCapturePhotoSuccess);
 document.addEventListener('me-open', handleModalOpen);
 document.addEventListener('me-close', handleModalClose);
 removeTextForm.addEventListener('submit', handleTextRemoveFormSubmit);
+removeConfirmationModal.addEventListener('click', removeConfirmationModalClose);
 
 galleryEl.querySelectorAll('button > img')?.forEach(image => {
   image.setAttribute('title', image.getAttribute('alt'));
